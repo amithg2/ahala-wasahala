403a81b068a9c86da5aeafd7192bbacd0e046177
Amit Hagag <amitadi91@gmail.com>
2024-11-15 10:24:55 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+    <router-view></router-view>
+  </main>
+</template>
+
+<script setup></script>
+
+<style scoped>
+
+
+main {
+  background: rgba(19, 19, 19, 1);
+  max-height: 1280px;
+  max-width: 720px;
+  height: 80vh;
+  width: 45vh;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
+  direction: rtl;
+}
+
+</style>
